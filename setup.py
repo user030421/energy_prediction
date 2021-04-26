6edb79669241bc1605a2929635fd69cd19d7919c
Brice Fotzo <fotzotalom@gmail.com>
2021-04-26 10:51:50 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A web app to predict a factory energy consumption given its production volume.',
+    author='brice fotzo',
+    license='MIT',
+)
